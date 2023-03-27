7f7511f83c75825db4615cd70d51857af372ab44
Jarek <jarek_m04@wp.pl>
2023-03-27 20:49:11 +0200

    redux in progress

--- src/features/tasksSlice.ts
+++ src/features/tasksSlice.ts
@@ -1,7 +1,35 @@
-type TaskState = {
-  entities: [];
+import { createSlice, nanoid, PayloadAction } from '@reduxjs/toolkit';
+
+export type TaskState = {
+  entities: Task[];
+};
+
+type DraftTask = Pick<Task, 'title'>;
+
+const createTask = (draftTask: DraftTask): Task => {
+  return {
+    id: nanoid(),
+    ...draftTask,
+  };
 };
 
 const initialState: TaskState = {
   entities: [],
-}
+};
+
+const tasksSlice = createSlice({
+  name: 'tasks',
+  initialState,
+  reducers: {
+    addTask: (state, action: PayloadAction<DraftTask>) => {
+      const task = createTask(action.payload);
+      state.entities.unshift(task);
+    },
+    removeTask: (state) => state,
+  },
+});
+
+export const tasksReducer = tasksSlice.reducer;
+export const { addTask, removeTask } = tasksSlice.actions;
+
+export default tasksSlice;
